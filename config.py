ce5f2c83cc5cc4af28407c51eea1c74a3da09211
MarzukhAsjad <marz.asjad00@gmail.com>
2024-06-23 00:55:05 +0800

    Remove main.py and config.py from flask-app

--- config.py
+++
@@ -1,9 +0,0 @@
-# Configuration for main.py
-BLEND_PATH = r'C:\Users\User\Desktop\FYP\blender-utils\Xbot.blend'
-IMPORT_PATH = r'C:\Users\User\Desktop\FYP\Motions\ybot'
-RENDER_PATH = r'C:\Users\User\Desktop\FYP\renders'
-MOTIONS = []
-TOTAL_FRAMES = 200
-# Status and code for the notification receiver
-CODE = 'N'
-STATUS = -1
